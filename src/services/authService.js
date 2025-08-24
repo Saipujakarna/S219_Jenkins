36059c5881c84b48220f285dcbcc8b42e5cc5312
Saipujakarna <saipujakarna@gmail.com>
2025-08-24 10:42:14 +0530

    Test

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,15 +1,17 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:9090/ecommerce-1.0.0/auth/signup";
+
 export const login = async (username, password) => {
   const response = await axios.post(
     "http://localhost:9090/ecommerce-1.0.0/auth/login",
     { username, password }
   );
-  localStorage.setItem("token", response.data);
-  return response.data;
-};
 
 export const signup = async (username, email, password) => {
-  return axios.post(
-    "http://localhost:9090/ecommerce-1.0.0/auth/signup",
-    { username, email, password }
-  );
+  return axios.post(`${API_URL}`, { username, email, password });
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
 };
