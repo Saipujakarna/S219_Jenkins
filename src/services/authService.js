71ce337288a28f781a321bf8b3ad4ea0c9dbc800
Saipujakarna <saipujakarna@gmail.com>
2025-08-24 10:55:45 +0530

    TEST

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,17 +1,15 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:9090/ecommerce-1.0.0/auth/signup";
+const API_URL = "http://localhost:9090/ecommerce-1.0.0/auth";
 
 export const login = async (username, password) => {
-  const response = await axios.post(
-    "http://localhost:9090/ecommerce-1.0.0/auth/login",
-    { username, password }
-  );
-  return response; // don't forget to return the response
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  localStorage.setItem("token", response.data);
+  return response.data;
 };
 
 export const signup = async (username, email, password) => {
-  return axios.post(`${API_URL}`, { username, email, password });
+  return axios.post(`${API_URL}/signup`, { username, email, password });
 };
 
 export const logout = () => {
